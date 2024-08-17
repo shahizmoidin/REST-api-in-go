3fa13808eab990ef5ea3bce76f08cf0c881cbd81
Darshan Dinesh <darshandineshmp@gmail.com>
2024-08-17 18:19:36 +0530

    I have added create and update product

--- controllers/product_controllers.go
+++ controllers/product_controllers.go
@@ -24,3 +24,32 @@ func GetProduct(c *gin.Context) {
     }
     c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
 }
+
+func CreateProduct(c *gin.Context) {
+    var newProduct models.Product
+
+    if err := c.ShouldBindJSON(&newProduct); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    newProduct.ID = nextID
+    nextID++
+    products = append(products, newProduct)
+    c.JSON(http.StatusCreated, newProduct)
+}
+
+func UpdateProduct(c *gin.Context) {
+    id := c.Param("id")
+    for i, product := range products {
+        if productID := c.Param("id"); productID == id {
+            if err := c.ShouldBindJSON(&products[i]); err != nil {
+                c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+                return
+            }
+            c.JSON(http.StatusOK, products[i])
+            return
+        }
+    }
+    c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+}
