8a9884f0f14677951db96c4a2bf0e1ad773ccffd
shahizmoidin <shahizmoidin@gmail.com>
2024-08-24 21:02:15 +0530

    added main.go and updated go.mod for mongodb setup

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
-//basic routes without database
+
 import (
-    "restapiingo/routes"  
-    "github.com/gin-gonic/gin"
+	"context"
+	"log"
+	"restapiingo/controllers"
+	"restapiingo/routes"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 func main() {
-    
-    r := gin.Default()
 
-    
-    routes.SetupRoutes(r)
+	clientOptions := options.Client().ApplyURI("mongodb+srv://nnm23mc060:le3uh4DDWIjuDymr@cluster0.ghe7tvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-  
-    r.Run(":8080")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MongoDB Atlas!")
+
+	controllers.InitProductController(client, "restapiforgo", "products")
+
+	r := gin.Default()
+	routes.SetupRoutes(r)
+
+	r.Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,16 +1,15 @@
 package routes
 
 import (
-    "restapiingo/controllers"
-    "github.com/gin-gonic/gin"
+	"restapiingo/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
-
-    router.GET("/products", controllers.GetProducts)
-    router.GET("/products/:id", controllers.GetProduct)
-    router.POST("/products", controllers.CreateProduct)
-    router.PUT("/products/:id", controllers.UpdateProduct)
-    router.DELETE("/products/:id", controllers.DeleteProduct)
-	
+	router.GET("/products", controllers.GetProducts)
+	router.GET("/products/:id", controllers.GetProduct)
+	router.POST("/products", controllers.CreateProduct)
+	router.PUT("/products/:id", controllers.UpdateProduct)
+	router.DELETE("/products/:id", controllers.DeleteProduct)
 }
