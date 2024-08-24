b3b239165fc6faa85e2e4fb0aa25cf0a5cde5767
shahizmoidin <shahizmoidin@gmail.com>
2024-08-24 20:24:37 +0530

    updated model for mongodb database #5

--- models/product.go
+++ models/product.go
@@ -1,12 +1,11 @@
 package models
 
-import "gorm.io/gorm"
-
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
-    gorm.Model        
-    Name        string  `json:"name"`
-    Description string  `json:"description"`
-    Price       float64 `json:"price"`
-    Quantity    int     `json:"quantity"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name        string             `bson:"name,omitempty" json:"name"`
+	Description string             `bson:"description,omitempty" json:"description"`
+	Price       float64            `bson:"price,omitempty" json:"price"`
+	Quantity    int                `bson:"quantity,omitempty" json:"quantity"`
 }
