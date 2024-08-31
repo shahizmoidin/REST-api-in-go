70b4ff23a7ff3762f88e59fe9f6762770814283d
shahizmoidin <shahizmoidin@gmail.com>
2024-08-31 21:08:46 +0530

    updated user model for password hash #7

--- models/user.go
+++ models/user.go
@@ -3,7 +3,8 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID           primitive.ObjectID
-	Username     string
-	PasswordHash string
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name         string             `bson:"name" json:"name"`
+	Email        string             `bson:"email" json:"email"`
+	PasswordHash string             `bson:"password_hash" json:"-"`
 }
