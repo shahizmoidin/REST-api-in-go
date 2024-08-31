55a690abefd8d7769cca707b8654d780503f4557
athul <athulkharvi1234@gmail.com>
2024-08-31 15:36:05 +0530

    implemented user model #7

--- models/user.go
+++ models/user.go
@@ -3,8 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-    ID           primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-    Username     string             `bson:"username,omitempty" json:"username"`
-    Email        string             `bson:"email,omitempty" json:"email"`
-    PasswordHash string             `bson:"password_hash,omitempty" json:"password_hash"`
+	ID           primitive.ObjectID
+	Username     string
+	PasswordHash string
 }
