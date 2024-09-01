8a173aa0f7a01a6cd5f8d4d9dedd108b88df6a94
shahizmoidin <shahizmoidin@gmail.com>
2024-09-01 10:37:35 +0530

    updated user model for regular password field  #10

--- models/user.go
+++ models/user.go
@@ -6,5 +6,6 @@ type User struct {
 	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Name         string             `bson:"name" json:"name"`
 	Email        string             `bson:"email" json:"email"`
+	Password     string             `bson:"-" json:"password,omitempty"` // Plain text password, not stored in DB
 	PasswordHash string             `bson:"password_hash" json:"-"`
 }
